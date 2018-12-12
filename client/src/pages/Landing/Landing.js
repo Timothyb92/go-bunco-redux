72a37424c67d3eaff282198ff44d88cb36dfb15d
Timothy Bedsole <timbedsole@gmail.com>
2018-12-11 23:31:58 -0500

    Added state and toggle logic for log in and sign up buttons

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import './Landing.css';
 
 class Landing extends Component {
+  state = {
+    logInClicked: false,
+    signUpClicked: false
+  };
+
+  toggleLogIn = () => {
+    this.setState({
+      logInClicked: !this.state.logInClicked
+    });
+    if (this.state.signUpClicked) {
+      this.setState({
+        signUpClicked: false
+      });
+    }
+  };
+
+  toggleSignUp = () => {
+    this.setState({
+      signUpClicked: !this.state.signUpClicked
+    });
+    if (this.state.logInClicked) {
+      this.setState({
+        logInClicked: false
+      });
+    }
+  };
+
   render() {
     return (
       <div className="container-fluid">
@@ -12,12 +41,16 @@ class Landing extends Component {
         </div>
         <div className="row">
           <div className="col-sm-12 text-center p-0">
-            <button className="btn w-100 mt-5">Sign Up</button>
+            <button className="btn w-100 mt-5" onClick={this.toggleSignUp}>
+              Sign Up
+            </button>
           </div>
         </div>
         <div className="row">
           <div className="col-sm-12 text-center p-0">
-            <button className="btn w-100 mt-5">Log In</button>
+            <button className="btn w-100 mt-5" onClick={this.toggleLogIn}>
+              Log In
+            </button>
           </div>
         </div>
       </div>
