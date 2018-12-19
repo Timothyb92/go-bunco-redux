83bdab00b7bc33be1c893afcb43d8190f9345a06
Timothy Bedsole <timbedsole@gmail.com>
2018-12-19 11:19:59 -0500

    Added model and controller files. Lobby controller not yet coded out.

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+const db = require('../models');
+
+module.exports = {
+  findAll: (req, res) => {
+    db.User.find(req.query)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: (req, res) => {
+    db.User.findById(req.params.userName)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
+  create: (req, res) => {
+    const user = {
+      userName: req.body.userName,
+      password: req.body.password
+    };
+    db.User.create(user)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
+  verifyUser: (req, res) => {
+    db.User.findOne({
+      userName: req.body.userName
+    }).then(dbUser => {
+      if (dbUser.password === req.body.password) {
+        return res.json(dbUser._id);
+      } else {
+        return null;
+      }
+    });
+  }
+};
---
+++ models/lobby.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const lobbySchema = new Schema({
+  lobbyName: { type: String, unique: true },
+  lobbyOwner: { type: String },
+  players: { type: Array }
+});
+
+const Lobby = mongoose.model('lobby', lobbySchema);
+
+module.exports = Lobby;
